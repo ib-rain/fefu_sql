de39ea3ccb23b6ac86ed643160c4652d5a41b8fc
Ivan Orlov <38328757+ib-rain@users.noreply.github.com>
2022-04-21 16:40:11 +0300

    final (windows)

--- sql_3.sql
+++ sql_3.sql
@@ -570,3 +570,192 @@ order by 3;
 
 
 #3.5-9
+with get_step_count(module_id, student_name, step_count)
+as
+(
+    select module_id,
+        student_name,
+        count(distinct step_id) as step_count
+    from student
+    join step_student using(student_id)
+    join step using (step_id)
+    join lesson using(lesson_id)
+    where result = 'correct'
+    group by module_id, student_name
+)
+select module_id as Модуль,
+    student_name as Студент,
+    step_count as Пройдено_шагов,
+    round(step_count / max(step_count) over (PARTITION BY module_id ) * 100, 1) as Относительный_рейтинг
+from get_step_count
+order by 1, 4 desc, 2;
+
+
+#3.5-10
+with get_joined(student_name, m_l, step_position, submission_time)
+as 
+(
+    select student_name,
+        concat(m.module_id, ".", lesson_position) as m_l,
+        step_position,
+        submission_time
+    from student
+        join step_student s_s using(student_id)
+        join step s using(step_id)
+        join lesson l using(lesson_id)
+        join module m using(module_id)
+    where result = "correct"
+),
+get_joined_last(student_name, m_l, submission_time)
+as
+(
+    select student_name,
+        m_l,
+        max(submission_time) as submission_time
+    from get_joined
+    group by student_name, m_l
+),
+get_students_three(student_name)
+as
+(
+    select student_name
+    from get_joined_last
+    group by student_name
+    having count(distinct m_l) >= 3
+)
+select student_name as Студент,
+        m_l as Урок,
+        FROM_UNIXTIME(submission_time) as Макс_время_отправки,
+        IFNULL(CEILING((submission_time - lag(submission_time)
+                   OVER (PARTITION BY student_name
+                         order by submission_time)) / (60 * 60 * 24)), "-") as Интервал
+from get_joined_last
+where student_name in (select student_name from get_students_three);
+
+
+#3.5-11
+with get_joined
+as 
+(
+    select student_name,
+        m.module_id as module_id,
+        lesson_position,
+        step_position,
+        result,
+        submission_time,
+        rank() over win1 as attempt_rank,
+        submission_time - attempt_time as attempt_period
+    from student
+        join step_student s_s using(student_id)
+        join step s using(step_id)
+        join lesson l using(lesson_id)
+        join module m using(module_id)
+    where student_name = "student_59"
+    window win1
+    as 
+    (
+        partition by student_name, module_id, lesson_position, step_position
+            order by submission_time
+    )
+),
+avg_under_hour_attempt
+as
+(
+    select student_name,
+        avg(attempt_period) as avg_per
+    from get_joined
+    where attempt_period < 1 * 60 * 60
+    group by student_name
+),
+get_changed_per
+as
+(
+    select student_name,
+        module_id,
+        lesson_position,
+        step_position,
+        attempt_rank,
+        result,
+        if(attempt_period < 1 * 60 * 60, attempt_period, round(avg_per)) as changed_per
+    from get_joined
+    join avg_under_hour_attempt using(student_name)
+)
+select student_name as Студент,
+    concat(module_id, ".", lesson_position, ".", step_position) as Шаг,
+    attempt_rank as Номер_попытки,
+    result as Результат,
+    sec_to_time(changed_per) as Время_попытки,
+    round(100 * changed_per / sum(changed_per) over win2, 2) as Относительное_время
+from get_changed_per
+window win2
+as
+(
+    partition by student_name, module_id, lesson_position, step_position
+)
+order by module_id, lesson_position, step_position, 3;
+
+
+#3.5-12
+with get_student_steps
+as
+(
+    select student_name,
+        step_student_id,
+        step_id,
+        submission_time,
+        result
+    from student
+    join step_student using(student_id)
+),
+get_student_res_counts
+as
+(
+    select student_name,
+        step_id,
+        sum(if(result = "correct", 1, 0)) as cnt_cor
+    from get_student_steps
+    group by student_name, step_id
+),
+get_lag_result
+as
+(
+    select student_name,
+        step_id,
+        result as res,
+        lead(result) over win1 as next_res
+    from get_student_steps
+    window win1
+    as
+    (
+        partition by student_name, step_id
+        order by submission_time
+    )
+)
+
+select "I" as Группа,
+    student_name as Студент,
+    count(step_id) as Количество_шагов
+from get_lag_result
+where res = "correct"
+    and next_res = "wrong"
+group by student_name
+
+union
+
+select "II" as Группа,
+    student_name as Студент,
+    count(step_id) as Количество_шагов
+from get_student_res_counts
+where cnt_cor > 1
+group by student_name
+
+union
+
+select "III" as Группа,
+    student_name as Студент,
+    count(step_id) as Количество_шагов
+from get_student_res_counts
+where cnt_cor = 0
+group by student_name
+
+order by 1, 3 desc, 2 asc;
